57fa2889b726587ab0e8d24cb21cd5358e43ae70
akasmajhi <akas.majhi@gmail.com>
2025-08-10 18:30:10 +0530

    Minor Test case changes for faster execution

--- tests/src/helpers/test_file_readers.py
+++ tests/src/helpers/test_file_readers.py
@@ -1,4 +1,4 @@
 import src.helpers.file_readers as file_readers
 
 def test_get_local_data():
-    assert file_readers.get_local_data("PE", "11-Jun-2025", "21-Jun-2025").size > 0  
+    assert file_readers.get_local_data("PE", "11-Jun-2025", "14-Jun-2025").size > 0  
--- tests/src/test_core.py
+++ tests/src/test_core.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+# from datetime import datetime
 
 from src import core
-from src.constants import DATE_FMT
+# from src.constants import DATE_FMT
 def test_get_data():
     # TESTS for non-existing file type data fetch 
 
@@ -11,16 +11,16 @@ def test_get_data():
     # assert core.get_data("PE", '12-12-2025', '') is False
     # assert core.get_data('PE', '01-Jun-2024', datetime.today().strftime(DATE_FMT)) is not None
     # assert core.get_data("PE", '12-12-2025', '') is False
-    assert core.get_data('PE', '01-Jul-2025', datetime.today().strftime(DATE_FMT)) is not None
-    assert core.get_data('PE', '01-Jul-2024', '07-Jul-2024') is not None
+    # assert core.get_data('PE', '01-Jul-2025', datetime.today().strftime(DATE_FMT)) is not None
+    assert core.get_data('PE', '01-Jul-2024', '01-Jul-2024') is not None
     # assert core.get_data('PE', '01-Jul-2023', '05-Jul-2023') is not None
 
     # TESTS for BHAVCOPY data fetch 
 
     assert core.get_data(file_type='BHAVCOPY', 
                          start_date='04-Jul-2025', 
-                         end_date='07-Jul-2025') is not None
+                         end_date='04-Jul-2024') is not None
 
-    assert core.get_data(file_type='BHAVCOPY', 
-                         start_date='07-Jul-2024', 
-                         end_date=datetime.today().strftime(DATE_FMT)) is not None
+    # assert core.get_data(file_type='BHAVCOPY', 
+    #                      start_date='07-Jul-2024', 
+    #                      end_date=datetime.today().strftime(DATE_FMT)) is not None
